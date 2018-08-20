b116b244d5d7418fa88d248710a3794cd45a98c5
abanihashemi <itsaminbanihashemi@gmail.com>
2018-08-20 15:59:46 +0430

    Added : list of values have been saved.

--- app/src/main/java/app/sematech/training/DataBase/DataBaseActivity.java
+++ app/src/main/java/app/sematech/training/DataBase/DataBaseActivity.java
@@ -1,39 +1,56 @@
 package app.sematech.training.DataBase;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import app.sematech.training.R;
 
 public class DataBaseActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText name,family;
+    EditText name, family;
     Button save;
     DatabaseHandler db;
+    ListView list;
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_base);
         Intent intent = getIntent();
-        String MessagID = intent.getStringExtra("message");
-        db = new DatabaseHandler(this,"sematech.db",null,1);
+        mContext = this;
+        db = new DatabaseHandler(mContext, "sematech.db", null, 1);
+        list = (ListView) findViewById(R.id.list);
         name = (EditText) findViewById(R.id.name);
-        family = (EditText)findViewById(R.id.family);
-        save = (Button)findViewById(R.id.save);
+        family = (EditText) findViewById(R.id.family);
+        save = (Button) findViewById(R.id.save);
+        show();
         save.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
-            db.insert(name.getText().toString(),family.getText().toString());
-            Toast.makeText(this, "new user has been added", Toast.LENGTH_SHORT).show();
-            name.setText("");
-            family.setText("");
+        db.insert(name.getText().toString(), family.getText().toString());
+        Toast.makeText(this, "new user has been added", Toast.LENGTH_SHORT).show();
+        name.setText("");
+        family.setText("");
+        show();
+    }
+
+    void show() {
+
+        List<StudentModel> students = db.getStudents();
+        StudentsDatabaseAdapter adapter = new StudentsDatabaseAdapter(mContext,students);
+        list.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/app/sematech/training/DataBase/DatabaseHandler.java
+++ app/src/main/java/app/sematech/training/DataBase/DatabaseHandler.java
@@ -1,8 +1,12 @@
 package app.sematech.training.DataBase;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by abanihashemi on 8/7/2018.
@@ -10,11 +14,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
     String tableQuery = "" +
-            "CREATE TABLE students (" +
-            " _id INTEGER PRIMARY KEY AUTOINCREMENT,"  +
-            "name TEXT ," +
-            "family TEXT " +
-            ")";
+            " CREATE TABLE students ( " +
+            " id INTEGER PRIMARY KEY AUTOINCREMENT , " +
+            " name TEXT  ," +
+            " family TEXT " +
+            " )";
+
 
     public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -25,12 +30,29 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL(tableQuery);
     }
     public void insert(String name,String family){
-        String insertQuery = "INSERT INTO students (name , family)" +
-                "VALUES ('"+name+"','"+family+"')" ;
+        String insertQuery = " INSERT INTO students (name,family) " +
+                " VALUES(  '" + name + "' , '" + family + "'   )";
+
+
+
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL(insertQuery);
         db.close();
 
+    }
+    public List<StudentModel> getStudents(){
+        List<StudentModel> students = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        //Order by id DESC : sort Descending
+        Cursor cursor= db.rawQuery("SELECT name,family FROM students ORDER BY id DESC",null);
+        while(cursor.moveToNext()){
+            String name = cursor.getString(0);
+            String family = cursor.getString(1);
+            StudentModel std = new StudentModel(name,family);
+
+            students.add(std);
+        }
+        return students;
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
