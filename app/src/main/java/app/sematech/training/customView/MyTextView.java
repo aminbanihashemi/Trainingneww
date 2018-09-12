32ab903ba4a05b6cf7f087f566b0dcb1bb271f71
abanihashemi <itsaminbanihashemi@gmail.com>
2018-09-12 13:17:45 +0430

    97.06.21 version 2

---
+++ app/src/main/java/app/sematech/training/customView/MyTextView.java
@@ -0,0 +1,23 @@
+package app.sematech.training.customView;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.TextViewCompat;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MyTextView extends TextView {
+
+    public MyTextView(Context context) {
+        super(context);
+        Typeface irSans = Typeface.createFromAsset(context.getAssets(),"irsans_p30download.com.ttf");
+        this.setTypeface(irSans);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        Typeface irSans = Typeface.createFromAsset(context.getAssets(),"irsans_p30download.com.ttf");
+        this.setTypeface(irSans);
+    }
+}
