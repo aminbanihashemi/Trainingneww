0fe1f4af9dc2618479f87988cc21bb9e9d780724
abanihashemi <itsaminbanihashemi@gmail.com>
2018-09-22 16:01:43 +0330

    97.06.31 version 1

---
+++ app/src/main/java/app/sematech/training/Fragments/FragmentContainerActivity.java
@@ -0,0 +1,13 @@
+package app.sematech.training.Fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_container);
+    }
+}
