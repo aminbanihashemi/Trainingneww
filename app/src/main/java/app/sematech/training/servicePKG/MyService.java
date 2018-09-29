62fd2dab4dc837b8eed4930c772caa9f701f235c
abanihashemi <itsaminbanihashemi@gmail.com>
2018-09-29 16:25:23 +0330

    97.07.07 version 1

--- app/src/main/java/app/sematech/training/servicePKG/MyService.java
+++ app/src/main/java/app/sematech/training/servicePKG/MyService.java
@@ -2,18 +2,53 @@ package app.sematech.training.servicePKG;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MyService extends Service {
-
+    String TAG="my_service";
     @Override
     public void onCreate() {
+
         super.onCreate();
+//        Toast.makeText(this, "Creaaaaaaate", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreate: ");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+        String url = "http://android.programmerguru.com/wp-content/uploads/2013/04/hosannatelugu.mp3";
+
+        MediaPlayer mPlayer = new MediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try{
+            mPlayer.setDataSource(url);
+        }catch (Exception e){
+
+        }
+        try{
+
+            mPlayer.prepare();
+        }catch (Exception e){
+
+        }
+
+        mPlayer.start();
+
+
+
+
+
+//        Toast.makeText(this, "StartCommaaaand", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, intent.getStringExtra("txt"), Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStartCommand: ");
 //        stopSelf(); kill the Service
         return super.onStartCommand(intent, flags, startId);
     }
--- app/src/main/java/app/sematech/training/servicePKG/ServiceStarterActivity.java
+++ app/src/main/java/app/sematech/training/servicePKG/ServiceStarterActivity.java
@@ -6,20 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import app.sematech.training.R;
 
 public class ServiceStarterActivity extends AppCompatActivity {
     Context mContext;
+    EditText txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_starter);
         mContext = this;
+        txt = (EditText) findViewById(R.id.txt);
         findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent serviceIntent = new Intent(mContext,MyService.class);
+                serviceIntent.putExtra("txt",txt.getText().toString());
                 startService(serviceIntent);
             }
         });
